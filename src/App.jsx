3f8541dcc08c929fd3412632dff3bef8d5d3126f
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-03 09:17:52 +0100

    Add brand logo and update App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Header from "./componet/header";
 
 function App() {
   return (
-    <>
+    <div style={{ backgroundColor: "hsl(227, 75%, 14%)" }}>
       <Header />
-    </>
+    </div>
   );
 }
 
