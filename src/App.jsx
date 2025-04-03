6d464814d852c77f8375a0a6d3955865b5bd4ce6
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-04 00:15:20 +0100

    Refactor App component to use useLocalStorage for dark mode state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,15 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Header from "./componet/header";
+import useLocalStorage from "use-local-storage";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
   return (
     <div
       style={{ backgroundColor: "var(--background-color)" }}
       className="h-screen"
       data-theme={darkMode ? "dark" : "light"}
     >
-      {/* <header className="body-font max-w-11/12 justify-center items-center mx-auto  pt-5">
-        <div
-          className="container mx-auto flex flex-row items-center rounded-2xl pl-3 pr-3"
-          style={{ backgroundColor: "var( --heading-background-color)" }}
-        >
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <img
-              src="/public/images/brand3d_logo-removebg-preview.png"
-              height="70px"
-              width="70px"
-              alt="brand3d logo"
-              className="rounded-full"
-            />
-            <span className="ml-3 text-xl text-black dark:text-white ">
-              Extensions
-            </span>
-          </a>
-          <button
-            className="inline-flex items-center ml-auto"
-            onClick={() => setDarkMode(!darkMode)}
-          >
-            {darkMode ? (
-              <img src="/images/icon-sun.svg" alt="icon-sun" />
-            ) : (
-              <img src="/images/icon-moon.svg" alt="icon-moon" />
-            )}
-          </button>
-        </div>
-      </header> */}
       <Header darkMode={darkMode} setDarkMode={setDarkMode} />
     </div>
   );
