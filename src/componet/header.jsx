f58d84e72b4f0265a9f0bbf479ebb5f47ab71c95
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-03 23:55:35 +0100

    Add Header component with dark mode toggle functionality

---
+++ src/componet/header.jsx
@@ -0,0 +1,33 @@
+export default function Header({darkMode, setDarkMode}) {
+  return (
+    <header className="body-font max-w-11/12 justify-center items-center mx-auto  pt-5">
+      <div
+        className="container mx-auto flex flex-row items-center rounded-2xl pl-3 pr-3"
+        style={{ backgroundColor: "var( --heading-background-color)" }}
+      >
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <img
+            src="/public/images/brand3d_logo-removebg-preview.png"
+            height="70px"
+            width="70px"
+            alt="brand3d logo"
+            className="rounded-full"
+          />
+          <span className="ml-3 text-xl text-black dark:text-white ">
+            Extensions
+          </span>
+        </a>
+        <button
+          className="inline-flex items-center ml-auto"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          {darkMode ? (
+            <img src="/images/icon-sun.svg" alt="icon-sun" />
+          ) : (
+            <img src="/images/icon-moon.svg" alt="icon-moon" />
+          )}
+        </button>
+      </div>
+    </header>
+  );
+}
