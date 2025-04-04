1ccab9a47f66aa8e4fff4bf0c2c21753d257788e
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-04 01:23:32 +0100

    Update App component to initialize dark mode based on system preferences and add Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-// import { useState } from "react";
+import Card from "./componet/card";
 import Header from "./componet/header";
 import useLocalStorage from "use-local-storage";
 
 function App() {
-  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
+  const prefences = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", prefences);
   return (
     <div
       style={{ backgroundColor: "var(--background-color)" }}
-      className="h-screen"
       data-theme={darkMode ? "dark" : "light"}
     >
       <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Card />
     </div>
   );
 }
